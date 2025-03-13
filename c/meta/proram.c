e7da9e761b360c6838acc27be5b19e3ae9073651
bidanee <dleksql119@gmail.com>
2025-03-13 12:19:05 +0900

    create Makefile_study

---
+++ c/meta/proram.c
@@ -0,0 +1,13 @@
+#include <stdio.h> // system library < >, User library " "
+
+void main() {
+  printf("Hello World~!!\n");
+}
+
+/* 
+리눅스 시스템이 아닐때
+int main(void){
+  printf("Hello World~!!\n");
+  return 0;
+}
+*/
