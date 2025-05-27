9c5ee0dcf7bd061db46786887015ffc61e3f1623
bidanee <dleksql119@gmail.com>
2025-05-27 02:27:54 +0000

    add study: langchain

---
+++ ai-1/3_langchain/p214_app.py
@@ -0,0 +1,24 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import ( SystemMessage, HumanMessage, AIMessage)
+
+chat_llm=ChatOpenAI(
+  model = 'gpt-3.5-turbo',
+  temperature = 0,
+)
+
+message = [
+  HumanMessage(content="고양이의 울음소리는?")
+]
+
+result = chat_llm(message)
+print('-'* 50)
+print(result)
+
+prompts = ["고양이의 울음소리는?","까마귀의 울음소리는?"]
+
+for i, prompt in enumerate(prompts):
+  message = [HumanMessage(content=prompt)]
+
+result = chat_llm(message)
+print('-' * 50)
+print(f"result {i} : {result.content}")
