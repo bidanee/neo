7d54f6558e235cec4dce80a9371e907c033851ca
bidanee <dleksql119@gmail.com>
2025-05-29 05:14:49 +0000

    add study: edit

--- ai-2/6_youtube/data/p229_app.py
+++ ai-2/6_youtube/data/p229_app.py
@@ -2,21 +2,24 @@ import yt_dlp
 
 def get_youtube_video_info(video_url):
     ydl_opts = {
-        'cookies' : './data/cookies.txt',
+        # 'cookies' :'./cookies.txt',
         'noplaylist': True,
         'quiet': True,
-        'no_warnings':True
+        'no_warnings': True
     }
-    
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        video_info = ydl.extract_info(video_url, download=False)
-        video_id = video_info['id']
-        title = video_info['title']
-        upload_date = video_info['upload_date']
-        channel = video_info['channel']
-        duration = video_info['duration_string']
-        
-    return video_id, title, upload_date, channel, duration
 
-video_url = 'https://www.youtube.com/watch?v=Th84afHZ8XA'
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            video_info = ydl.extract_info(video_url, download=False)
+            return {
+
+                'title' : video_info.get('title'),
+                'upload_date': video_info.get('upload_date'),
+                'channel' : video_info.get('channel'),
+                'duration' : video_info.get('duration_string'),
+            }
+    except Exception as e:
+        return f'Error: {e}'
+
+video_url = 'https://www.youtube.com/watch?v=pSJrML-TTmI'
 print(get_youtube_video_info(video_url))
